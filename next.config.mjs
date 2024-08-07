429913810fd2b7675b5f970b7099fa2b91313bb5
Dothat65 <meemoahmed2004@gmail.com>
2024-08-06 22:35:34 -0400

    hid API keys

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+// Import the dotenv package
+import { config } from 'dotenv';
+config();
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    VITE_APP_API_KEY: process.env.VITE_APP_API_KEY,
+    VITE_APP_AUTH_DOMAIN: process.env.VITE_APP_AUTH_DOMAIN,
+    VITE_APP_PROJECT_ID: process.env.VITE_APP_PROJECT_ID,
+    VITE_APP_STORAGE_BUCKET: process.env.VITE_APP_STORAGE_BUCKET,
+    VITE_APP_MESSAGING_SENDER_ID: process.env.VITE_APP_MESSAGING_SENDER_ID,
+    VITE_APP_APP_ID: process.env.VITE_APP_APP_ID,
+    VITE_APP_MEASUREMENT_ID: process.env.VITE_APP_MEASUREMENT_ID,
+  },
+};
 
 export default nextConfig;
--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-require('dotenv').config();
-
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: process.env.VITE_APP_API_KEY,
+  authDomain: process.env.VITE_APP_AUTH_DOMAIN,
+  projectId: process.env.VITE_APP_PROJECT_ID,
+  storageBucket: process.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_APP_APP_ID,
+  measurementId: process.env.VITE_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
